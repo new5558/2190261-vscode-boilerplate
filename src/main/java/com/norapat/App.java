f0deb8743f3d2735e8ac3de6e296a7e9711c5733
Norapat Buppodom <6131796821@student.chula.ac.th>
2020-01-24 07:55:12 +0000

    build: init project

---
+++ src/main/java/com/norapat/App.java
@@ -0,0 +1,13 @@
+package com.norapat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
